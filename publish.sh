910a90b2c6ca57ab731bff758c0e2246303a3762
Ignasi Barrera <nacx@apache.org>
2019-03-13 11:38:11 +0100

    Publish site to GH pages

--- publish.sh
+++ publish.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-PUBLIC_DIR=public
-
 set -e 
 
-if [[ $(git status -s) ]]; then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo "Deleting old publication"
-rm -rf ${PUBLIC_DIR}
-mkdir ${PUBLIC_DIR}
-git worktree prune
-rm -rf .git/worktrees/${PUBLIC_DIR}/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B master ${PUBLIC_DIR} origin/master
-
-echo "Removing existing files"
-rm -rf ${PUBLIC_DIR}/*
-
-echo "Generating site..."
 make render
 
-echo "Updating gh-pages branch"
-cd ${PUBLIC_DIR}
-git add --all
-git commit -m "Publishing to gh-pages (publish.sh)"
+pushd public
+git add .
+git commit -m "Publish site"
 git push origin master
+popd
