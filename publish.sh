7cadddae6067b7b3b4d732f6ec0e3e251ac6b04d
Ignasi Barrera <nacx@apache.org>
2019-03-13 11:24:01 +0100

    Publish site to master

--- publish.sh
+++ publish.sh
@@ -16,16 +16,16 @@ git worktree prune
 rm -rf .git/worktrees/${PUBLIC_DIR}/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages ${PUBLIC_DIR} origin/gh-pages
+git worktree add -B master ${PUBLIC_DIR} origin/master
 
 echo "Removing existing files"
 rm -rf ${PUBLIC_DIR}/*
 
 echo "Generating site..."
-hugo
+make render
 
 echo "Updating gh-pages branch"
 cd ${PUBLIC_DIR}
 git add --all
 git commit -m "Publishing to gh-pages (publish.sh)"
-git push origin gh-pages
+git push origin master
